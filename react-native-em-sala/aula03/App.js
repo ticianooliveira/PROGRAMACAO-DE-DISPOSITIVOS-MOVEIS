4315fa0ecc571f96fb16fadfd02ad193df0d407c
Ticiano <ticianoodonto@gmail.com>
2025-03-14 21:07:33 -0300

    aula 03
    Tela de LoginScreens

---
+++ react-native-em-sala/aula03/App.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import LoginScreens from './src/screens/LoginScreens'
+
+const App = () => {
+  return (
+      <LoginScreens />
+  )
+}
+
+export default App
---
+++ react-native-em-sala/aula03/src/screens/LoginScreens.js
@@ -0,0 +1,55 @@
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Button } from 'react-native'
+import React from 'react'
+import { useState } from 'react'
+import {AntDesign, Ionicons} from '@expo/vector-icons'
+
+const LoginScreens = () => {
+
+    const {username, setUsername} = useState (" ")
+    const {password, setPassword} = useState (" ")
+
+  return (
+    <View styles={styles.container}>
+      <Text styles ={styles.title}>🔐 Login</Text>
+
+      <TextInput
+        placeholder="Digite seu nome"
+        style={styles.input}
+        value={username}
+        onChangeText={setUsername}
+      />
+        <TextInput
+            placeholder="Digite sua senha"
+            style={styles.input}
+            secureTextEntry
+            value={password}
+            onChangeText={setPassword}
+    />
+
+    <Button title="Entrar"/>
+
+    <Text style={styles.orText}>Ou entre com</Text>
+    {/* Botão de login com Google */}
+    <TouchableOpacity style={styles.socialButton}>
+        <AntDesign name="google" size={24} color="white" />
+        <Text style={styles.socialText}>Entrar com Google</Text>
+    </TouchableOpacity>
+
+    {/* Botão do Github */}
+    <TouchableOpacity style={[styles.socialButton, styles.githubButton]}>
+        <Ionicons name="logo-github" size={24} color="white" />
+        <Text style={styles.socialText}>Entrar com GitHub</Text> 
+    </TouchableOpacity>
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+    container: { flex: 1, justifyContent: 'center', alignItems: 'center', padding: 20 },
+    title: { fontSize: 24, fontWeight: 'bold', marginBottom: 20 },
+    input: { width: '100%', borderBottomWidth: 1, marginBottom: 20, padding: 8 },
+    orText: { marginVertical: 20, fontSize: 16, color: '#555' },
+    socialButton: { flexDirection: 'row', alignItems: 'center', justifyContent: 'center', width: '100%', padding: 10, backgroundColor: '#DB4437', borderRadius: 5, marginVertical: 5 },
+    socialText: { color: 'white', fontSize: 16, marginLeft: 10 },
+    githubButton: { backgroundColor: '#333' }
+    })
+export default LoginScreens

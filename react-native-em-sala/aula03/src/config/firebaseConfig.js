48619db382391c668ffe73765cbb0a152195e776
Ticiano <ticianoodonto@gmail.com>
2025-04-10 20:30:40 -0300

    Atualizações
    firebaseConfig
    .end
    gitIgnore

--- react-native-em-sala/aula03/src/config/firebaseConfig.js
+++ react-native-em-sala/aula03/src/config/firebaseConfig.js
@@ -1,21 +1,30 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  MEASUREMENT_ID,
+} from "@env";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD5An-v1wCjiR_ZCbZx3zro97etAMYBqmo",
-  authDomain: "tech-cursos-10f42.firebaseapp.com",
-  projectId: "tech-cursos-10f42",
-  storageBucket: "tech-cursos-10f42.firebasestorage.app",
-  messagingSenderId: "32325583150",
-  appId: "1:32325583150:web:b5427e1a826868e9923abf",
-  measurementId: "G-KSHPQ74X0Y"
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
+export { db };
